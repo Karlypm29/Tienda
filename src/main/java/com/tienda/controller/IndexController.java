7bd17ebc687ac1034255ddf802f6f73586af3f4b
Usuario <Usuario@DESKTOP-D1O15PC>
2022-06-15 17:50:14 -0600

    Quinto push del proyecto Tienda Semana 05

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,13 +1,14 @@
 package com.tienda.controller;
 
-import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -17,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Slf4j
 public class IndexController {
     @Autowired
-    private ClienteDao clienteDao;
+    private ClienteService clienteService;
     @GetMapping("/")
     public String Inicio(Model model) {
         log.info("Ahora utilizamos MVC");
@@ -31,9 +32,34 @@ public class IndexController {
 //        model.addAttribute("cliente", cliente);
 //        model.addAttribute("clientes", clientes);
         
-        var clientes = clienteDao.findAll();
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         
+        
         return "index";
+    }
+    
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/tienda/service/ClienteService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import com.tienda.domain.Cliente;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface ClienteService {
+    
+    public List<Cliente> getClientes();
+    
+    public Cliente getCliente(Cliente cliente);
+    
+    public void save(Cliente cliente);
+    
+    public void delete(Cliente cliente);
+    
+}
